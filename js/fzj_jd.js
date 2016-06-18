83904899b8ca1f1e9bd42f8cd0cabacdbe391bee
fuzongjian <2917066078@qq.com>
2016-06-18 20:49:12 +0800

    完成图片轮播功能

--- js/fzj_jd.js
+++ js/fzj_jd.js
@@ -43,9 +43,71 @@ $(function () {
 		$(this).find('a').css({'color':'#005EA7'})
 	});
 
+	// 定时器方法设置图片轮播
+	var c =0;
+	function autoRun() {
+		console.log(c);
+		c++;
+		c=(c==6)?0:c;
+		$('#middle .logo1 img').eq(c).fadeIn(500).siblings('img').hide();
+		$('#middle .logo1 ul li').eq(c).css({'background':'#E4393C'}).siblings('li').css({'background':'#999'});
+	}
+	var re = setInterval(autoRun,2000);
+	// 给鼠标加入移入事件
+	$('#middle .logo1 ul li').mouseenter(function() {
+		clearInterval(re);//清理定时器
+		c = $(this).index();//获得鼠标移入的li的序号
+		$('#middle .logo1 img').eq(c).fadeIn(500).siblings('img').hide();	// 让c号图片显示，兄弟图片隐藏
+		$('#middle .logo1 ul li').eq(c).css({'background':'#E4393C'}).siblings('li').css({'background':'#999'});	//c号li变红，兄弟li变灰							
+	});
 
+	// 给鼠标加入移出事件
+	$('#middle .logo1 ul li').mouseleave(function() {
+		re=setInterval(autoRun,1000);
+		/* Act on the event */
+	});
 
+	// 左侧二级菜单区域开始
+	$('#middle .left .l').hover(function () {
+		$(this).find('.box_hide').show();
+		$(this).find('p').addClass('cur');
+	},function () {
+		$(this).find('.box_hide').hide();
+		$(this).find('p').removeClass('cur');
+	});
+	// 左侧二级菜单区域结束
 
+	// 图片轮播下面区域开始
+	$('#middle .logo2 .left1').hover(function () {
+		$(this).css({'background':'#EEEEEE'});//鼠标移入之后
+		$('#middle .logo2 .zuo').css({'background':'url(images/15.png) no-repeat -6px -5px'});
+	},function () {
+		$(this).css({'background':'white'});//鼠标移开之后
+		$('#middle .logo2 .zuo').css({'background':'url(images/1.png) no-repeat -91px -50px'})
+	});
 
-	
+	$('#middle .logo2 .left2').hover(function() {
+		$(this).css({'background':' #EEEEEE'});
+		$('#middle .logo2 .you').css({'background':'url(images/15.png) no-repeat -6px -42px'});
+	}, function() {
+		$(this).css({'background':'white'});
+		$('#middle .logo2 .you').css({'background':'url(images/1.png) no-repeat -60px -50px'});
+	});
+
+	// 鼠标添加点击事件让图片左右移动
+	var l = 0;
+	$('#middle .logo2 .left1').click(function () {
+		l++;
+		if (l==6) {
+			l=0;
+		}
+		$('#middle .logo2 .b').stop().animate({'right':-606*l},600);
+	});
+	$('#middle .logo2 .left2').click(function () {
+		l--;
+		if (l==-1) {
+			l=5;
+		}
+		$('#middle .logo2 .b').stop().animate({'right':-606*l},600);
+	});
 })
